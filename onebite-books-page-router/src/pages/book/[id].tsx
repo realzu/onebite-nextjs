954ab56498bcb8c28ae21950b0dfa57819302d3c
realzu <jinhh501@naver.com>
2025-08-17 21:43:03 +0900

    feat: SSG (Static Site Generation) 실습

--- onebite-books-page-router/src/pages/book/[id].tsx
+++ onebite-books-page-router/src/pages/book/[id].tsx
@@ -1,12 +1,31 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: true,
+    // false : 404 Not found
+    // blocking : SSR 방식
+    // true : SRR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: { book },
@@ -15,7 +34,12 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return "로딩 중 입니다.";
+  }
   if (!book) {
     return "문제가 발생했습니다. 다시 시도하세요.";
   }
--- onebite-books-page-router/src/pages/index.tsx
+++ onebite-books-page-router/src/pages/index.tsx
@@ -3,13 +3,14 @@ import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-rendom-books";
 
 // SSR (Server Side Rendering)
 // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
+  console.log("index page");
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -26,7 +27,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
