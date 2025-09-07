b1f8eca5eb24dceff34ea70b7faca8214a11eb04
realzu <jinhh501@naver.com>
2025-09-07 13:01:59 +0900

    feat: setting for data fetching

--- onebite-books-app-router/src/app/(with-searchbar)/search/page.tsx
+++ onebite-books-app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,17 +1,18 @@
-import ClientComponent from "@/app/components/client-component";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ q: string }>;
+  searchParams: Promise<{ q?: string }>;
 }) {
   const { q } = await searchParams;
+
   return (
     <div>
-      Search Page : {q}
-      <ClientComponent>
-        <></>
-      </ClientComponent>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
