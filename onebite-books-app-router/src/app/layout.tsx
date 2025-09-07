0c920d84124b365bd8c959299e5b04df477d0f39
realzu <jinhh501@naver.com>
2025-09-07 14:18:54 +0900

    feat: data fetching

--- onebite-books-app-router/src/app/layout.tsx
+++ onebite-books-app-router/src/app/layout.tsx
@@ -1,6 +1,26 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+
+  if (!response.ok) {
+    return <footer>제작 @winterlood</footer>;
+  }
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @winterlood</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다</div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +35,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
